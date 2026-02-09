f01f6ffb97fc6f85ef91a3f3511da585b329b48f
ashish-ankam <ashishankam1999@gmail.com>
2026-02-09 20:31:19 +0530

    added destroy method

--- src/main/java/com/servlets/ProductServlet.java
+++ src/main/java/com/servlets/ProductServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Servlet implementation class ProductServlet
@@ -17,6 +19,7 @@ public class ProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
 	private Connection con;
+	private PreparedStatement stmt;
 	
 	public void init(ServletConfig config) {
 		
@@ -24,20 +27,24 @@ public class ProductServlet extends HttpServlet {
 	}
 	
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	public void destroy() {
+		
+	  try {
+		  con.close();
+		stmt.close();
+	} catch (SQLException e) {
+		
+		e.printStackTrace();
+	}	
 	}
 
 }
